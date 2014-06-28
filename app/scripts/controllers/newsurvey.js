70ff30a523b8794b84617407037b46c876da84f7
Till Reiter <till.reiter@gmail.com>
2014-06-28 20:37:32 +0200

    add dynamic process bar

--- app/scripts/controllers/newsurvey.js
+++ app/scripts/controllers/newsurvey.js
@@ -1,26 +1,44 @@
 'use strict';
 
 angular.module('dmocrtzrApp')
-    .controller('NewSurveyCtrl', ['$scope', function ($scope) {
-        $scope.sex = "";
-        $scope.votingStyle = "";
-        $scope.votingPrivacy = "";
-        $scope.votingPreSurveyIncluded = "";
-        $scope.votingPreSurveyDuration = "";
-        $scope.votingSurveyDuration = "";
-        $scope.votingQuestions = [];
+  .controller('NewSurveyCtrl', ['$scope', function ($scope) {
+    $scope.sex = "";
+    $scope.votingStyle = "";
+    $scope.votingPrivacy = "";
+    $scope.votingPreSurveyIncluded = "";
+    $scope.votingPreSurveyDuration = "";
+    $scope.votingSurveyDuration = "";
+    $scope.includeExtraQestions = "";
 
-        $scope.progress = 33;
-//        $scope.addProgress = function () {
-//            $scope.progress += (100/7)
-//        }
+    $scope.questionsTotal = 7;
 
+    $scope.progress = 0;
+    $scope.setProgress = function () {
+      function calculateProgress (questionNumber){
+        $scope.progress = Math.floor((questionNumber/ $scope.questionsTotal)*100);
+        return $scope.progress;
+      }
 
-        $scope.submitForm = function () {
-//        push to server
-            console.log('Hi I am here');
-            console.log($state.is("app.newSurvey"));
-            $state.go("^.preSurvey")
-        }
-
-    }]);
+      if ($scope.sex !== "") {
+        calculateProgress(1);
+      }
+      if ($scope.votingStyle !== "") {
+        calculateProgress(2);
+      }
+      if ($scope.votingPrivacy !== "") {
+        calculateProgress(3);
+      }
+      if ($scope.votingPreSurveyIncluded !== "") {
+        calculateProgress(4);
+      }
+      if ($scope.votingPreSurveyDuration !== "") {
+        calculateProgress(5);
+      }
+      if ($scope.votingSurveyDuration !== "") {
+        calculateProgress(6);
+      }
+      if ($scope.includeExtraQestions !== "") {
+        calculateProgress(7);
+      }
+    }
+  }]);
